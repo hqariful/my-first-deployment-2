6839f131b790ea279f370c6b7f364290511045d5
hqariful <hq.ariful@gmail.com>
2020-09-28 16:58:45 +0600

    added env variable

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ db.loadDatabase();
 
 app.set("view-engine", "ejs");
 
-app.listen(3000, () => {
-  console.log("listening to port 3000...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening to port ${port}...`);
 });
 var name;
 app.get("/", checkData, (req, res) => {
